8e6ec4a31bb20fc21e4eab3481909d1616fb83cc
mparje <126131806+mparje@users.noreply.github.com>
2023-05-14 22:00:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -5,15 +6,21 @@ from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 
-# Replace book.pdf with any pdf of your choice
-loader = UnstructuredPDFLoader("book.pdf")
-pages = loader.load_and_split()
-embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
+def run_question_answering():
+    # Replace book.pdf with any pdf of your choice
+    loader = UnstructuredPDFLoader("book.pdf")
+    pages = loader.load_and_split()
+    embeddings = OpenAIEmbeddings()
+    docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
 
-# Choose any query of your choice
-query = "Who is Rich Dad?"
-docs = docsearch.get_relevant_documents(query)
-chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-output = chain.run(input_documents=docs, question=query)
-print(output)
+    # Choose any query of your choice
+    query = st.text_input("Enter your question")
+    if query:
+        docs = docsearch.get_relevant_documents(query)
+        chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+        output = chain.run(input_documents=docs, question=query)
+        st.write(output)
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    run_question_answering()
